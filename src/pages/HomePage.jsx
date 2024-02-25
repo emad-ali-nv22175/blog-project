4255f016bcc4e9354a7616dd320dadf16cd4835c
emad-ali-nv22175 <149559151+emad-ali-nv22175@users.noreply.github.com>
2024-02-25 08:37:22 +0000

    setup project 2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import MainLayout from '../components/components/MainLayout'
 
 const HomePage = () => {
-  return <MainLayout></MainLayout>
+  return <MainLayout>
+
+  </MainLayout>;
+
 };
 
 export default HomePage
-
