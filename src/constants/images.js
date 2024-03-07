1263f1383f3c95eb9b647fa913d80d5bd037e74e
emad-ali-nv22175 <149559151+emad-ali-nv22175@users.noreply.github.com>
2024-03-07 13:02:08 +0000

    video 7 done

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,14 +1,17 @@
 import Logo from '../assets/Logo.svg';
 
-import HeroImage from '../assets/HeroImage.svg'
+import HeroImage from '../assets/HeroImage.svg';
 
-import post1Image from '../assets/posts/post1Image.svg';
-
+import post1Image from '../assets/posts/post1Image.svg.png';
+import CtaImage from "../assets/CtaImage.png"
+import postprofileimage from '../assets/posts/post-profile.svg';
 const images = {
 
     Logo,
-
+    CtaImage,
     HeroImage,
+    post1Image,
+    postprofileimage,
 
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import MainLayout from '../components/MainLayout'
 import Hero from './Container/Hero'
+import Articles from './Container/Articles';
+import CTA from './Container/CTA';
+
 
 const HomePage = () => {
   return (
   <MainLayout>
     <Hero/>
+    <Articles />
+    <CTA />
   </MainLayout>
+
     )
  
 };
