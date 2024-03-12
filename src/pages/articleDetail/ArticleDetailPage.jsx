432b65d2f6af4b901226d318dc324fee6488109f
emad-ali-nv22175 <149559151+emad-ali-nv22175@users.noreply.github.com>
2024-03-12 13:39:26 +0000

    VIDOE 11 ICONS

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -82,12 +82,21 @@ const ArticleDetailPage = () => {
           </div>
           <CommentsContainer className="mt-10" logginedUserId="a" />
         </article>
+       
+
         <SuggestedPosts
           header="Latest Article"
           posts={postsData}
           tags={tagsData}
           className="mt-8 lg:mt-0 lg:max-w-xs"
+         
         />
+        <div classname='mt-7'>
+         <h2 className="font-roboto  font-medium text-dark-hard mb-4 mb:text-xl">
+          Share on:
+         </h2> 
+       
+        </div>
       </section>
     </MainLayout>
   );
